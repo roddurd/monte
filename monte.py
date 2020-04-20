d7a165fff218c1dc3e572c6c328fd7d3ccdce0c8
Roddur Dasgupta <roddurd@gmail.com>
2020-04-20 16:24:12 -0700

    first take

--- monte.py
+++ monte.py
@@ -1,2 +1,14 @@
-import random
+import math, numpy, random
 
+def estimate_integral(func, a, b):
+    samples = 100
+    ys = [(b-a)*func(*math.random()) for s in samples]
+    return ys.mean()
+
+def func1(x):
+    return math.exp(x+x**2)
+
+def func2(x):
+    return numpy.sinc(x)
+
+print(estimate_integral(func1, -2, 2))
