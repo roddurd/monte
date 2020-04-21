3f0169ee2f828c943b3cee8c59647bb78a9a3e72
Roddur Dasgupta <roddurd@gmail.com>
2020-04-20 17:02:08 -0700

    X~U[a,b]

--- monte.py
+++ monte.py
@@ -1,9 +1,9 @@
 import math, numpy, random
 
 def estimate_integral(func, a, b):
-    samples = 100
-    ys = [(b-a)*func(random.random()) for s in range(samples)]
-    return sum(ys) / len(ys)
+    samples = 1000000
+    ys = [func(random.uniform(a,b)) for s in range(samples)]
+    return (b-a)*sum(ys) / samples
 
 def func1(x):
     return math.exp(x+x**2)
@@ -11,4 +11,7 @@ def func1(x):
 def func2(x):
     return numpy.sinc(x)
 
-print(estimate_integral(func1, -2, 2))
+def func3(x):
+    return 4*(1-x**2)**(0.5)
+
+print(estimate_integral(func2, 0,4*math.pi))
